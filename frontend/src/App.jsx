d111ee19105ba2ad1c575ab6b6c82c51937f0bd5
pardipYadav <213sunilyadav@gmail.com>
2025-07-14 07:27:25 +0530

    Added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+import Navbar from "./components/ui/shared/Navbar";
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+export default App;
